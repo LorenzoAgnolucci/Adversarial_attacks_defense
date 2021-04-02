4786b54b42c82518784302b8c8746eeff6e20ac1
LorenzoAgnolucci <lory.agnolucci@gmail.com>
2021-04-02 16:52:12 +0200

    remove useless folder

--- code_vmr/art/preprocessing/audio/__init__.py
+++
@@ -1,2 +0,0 @@
-from art.preprocessing.audio.l_filter.l_filter import LFilter
-from art.preprocessing.audio.l_filter.l_filter_pytorch import LFilterPyTorch
--- code_vmr/art/preprocessing/standardisation_mean_std/__init__.py
+++
@@ -1,5 +0,0 @@
-from art.preprocessing.standardisation_mean_std.standardisation_mean_std import StandardisationMeanStd
-from art.preprocessing.standardisation_mean_std.standardisation_mean_std_pytorch import StandardisationMeanStdPyTorch
-from art.preprocessing.standardisation_mean_std.standardisation_mean_std_tensorflow import (
-    StandardisationMeanStdTensorFlowV2,
-)
